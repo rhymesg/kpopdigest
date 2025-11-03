b0373f7474c661de50afa5fe337e134777211582
rhymesg <rhymesg@gmail.com>
2025-11-03 15:55:58 +0100

    adsense script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -40,6 +40,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8612922709654862"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>
         {children}
